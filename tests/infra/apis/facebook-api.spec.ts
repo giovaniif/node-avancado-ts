e8cc9bb20eea3baeb5d64d562876bea87d597285
Giovani Farias <riccog.25@gmail.com>
2021-08-12 20:35:25 -0300

    refactor: isolate SUT creation

--- tests/infra/apis/facebook-api.spec.ts
+++ tests/infra/apis/facebook-api.spec.ts
@@ -1,4 +1,4 @@
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 import { LoadFacebookUserApi } from '@/data/contracts/api'
 
@@ -35,13 +35,22 @@ namespace HttpGetClient {
 }
 
 describe('Facebook api', () => {
-  const clientId = 'any_client_id'
-  const clientSecret = 'any_client_secret'
+  let clientId: string
+  let clientSecret: string
+  let httpClient: MockProxy<HttpGetClient>
+  let sut: FacebookApi
+
+  beforeAll(() => {
+    clientId = 'any_client_id'
+    clientSecret = 'any_client_secret'
+    httpClient = mock()
+  })
+
+  beforeEach(() => {
+    sut = new FacebookApi(httpClient, clientId, clientSecret)
+  })
 
   it('should get app token', async () => {
-    const httpClient = mock<HttpGetClient>()
-    const sut = new FacebookApi(httpClient, clientId, clientSecret)
-
     await sut.loadUser({ token: 'any_client_token' })
 
     expect(httpClient.get).toHaveBeenCalledWith({
