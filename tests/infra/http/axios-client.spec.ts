a1c497e254251ef77799000fd819c44ef01b3336
Giovani Farias <riccog.25@gmail.com>
2021-08-12 21:52:54 -0300

    test: ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -44,5 +44,13 @@ describe('axios http client', () => {
 
       expect(result).toEqual('any_data')
     })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
