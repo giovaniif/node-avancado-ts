55be896ab7ee21a1a64fc934e0e8e700be7c472d
Giovani Farias <riccog.25@gmail.com>
2021-08-25 07:10:14 -0300

    refactor: isolate SUT creation

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -1,5 +1,5 @@
-import { newDb } from 'pg-mem'
-import { Column, Entity, getRepository, PrimaryGeneratedColumn } from 'typeorm'
+import { IBackup, newDb } from 'pg-mem'
+import { Column, Entity, getConnection, getRepository, PrimaryGeneratedColumn, Repository } from 'typeorm'
 
 import { LoadUserAccountRepository } from '@/data/contracts/repos'
 
@@ -34,36 +34,42 @@ class PgUser {
 
 describe('postgres user account repository', () => {
   describe('load', () => {
-    it('should return an account if email exists', async () => {
+    let sut: PgUserAccountRepository
+    let pgUserRepo: Repository<PgUser>
+    let backup: IBackup
+
+    beforeAll(async () => {
       const db = newDb()
       const connection = await db.adapters.createTypeormConnection({
         type: 'postgres',
         entities: [PgUser]
       })
       await connection.synchronize()
-      const pgUserRepo = getRepository(PgUser)
+      backup = db.backup()
+      pgUserRepo = getRepository(PgUser)
+    })
+
+    beforeEach(() => {
+      backup.restore()
+      sut = new PgUserAccountRepository()
+    })
+
+    afterAll(async () => {
+      await getConnection().close()
+    })
+
+    it('should return an account if email exists', async () => {
       await pgUserRepo.save({ email: 'existing_email' })
-      const sut = new PgUserAccountRepository()
 
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
-      await connection.close()
     })
 
     it('should return undefined if email does not exists', async () => {
-      const db = newDb()
-      const connection = await db.adapters.createTypeormConnection({
-        type: 'postgres',
-        entities: [PgUser]
-      })
-      await connection.synchronize()
-      const sut = new PgUserAccountRepository()
-
       const account = await sut.load({ email: 'new_email' })
 
       expect(account).toBeUndefined()
-      await connection.close()
     })
   })
 })
