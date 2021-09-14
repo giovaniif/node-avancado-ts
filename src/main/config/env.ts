825dbbfdccb808d367840329e672a88ab03d2b81
Giovani Farias <riccog.25@gmail.com>
2021-09-14 20:31:14 -0300

    test(integration): ensure fb api returns fb user if token is valid

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '1525222224543016',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '7a6c24cd750da131aa2c9560f03e9436'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,27 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('Facebook Api Integration Tests', () => {
+  it('should return a facebook user if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+
+    const fbUser = await sut.loadUser({ token: 'EAAVrLnjlvSgBAAeSZBmunXSKnE8ZCPgcx3eTexFgtVem8pKBlDyFa7eQ3A2HmWh5PR9U1XCaDuJ67MHf5G0M3wZATYp35kPsl7ZAe1YGKVsCVmPZBAMOE4sJIcZBFjFsLVyvfIfsn6qBAVsIcKsfBqUWxmzK7ceZCtoZAfVSJuAfb8BZAJcKiQazZC2IHNi9ld3PGu2fGOQigaPAZDZD' })
+
+    expect(fbUser).toEqual({
+      facebookId: '109612464782676',
+      email: 'giovani_zsgszcu_teste@tfbnw.net',
+      name: 'Giovani Teste'
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
