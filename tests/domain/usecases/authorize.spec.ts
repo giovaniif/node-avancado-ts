ee18115d5a8456a162f687a959e5f13a90b4fef8
Giovani Farias <riccog.25@gmail.com>
2021-09-28 13:52:11 -0300

    test: ensure Authorize calls TokenValidator with correct token

---
+++ tests/domain/usecases/authorize.spec.ts
@@ -0,0 +1,41 @@
+import { mock, MockProxy } from 'jest-mock-extended'
+
+export namespace TokenValidator {
+  export type Params = { token: string }
+}
+
+interface TokenValidator {
+  validateToken: (params: TokenValidator.Params) => Promise<void>
+}
+
+type Setup = (crypto: TokenValidator) => Authorize
+type Input = { token: string }
+type Authorize = (params: Input) => Promise<void>
+
+const setupAuthorize: Setup = crypto => {
+  return async params => {
+    await crypto.validateToken(params)
+  }
+}
+
+describe('Authorize', () => {
+  let crypto: MockProxy<TokenValidator>
+  let sut: Authorize
+  let token: string
+
+  beforeAll(() => {
+    token = 'any_token'
+    crypto = mock()
+  })
+
+  beforeEach(() => {
+    sut = setupAuthorize(crypto)
+  })
+
+  it('should call TokenValidator with correct params', async () => {
+    await sut({ token })
+
+    expect(crypto.validateToken).toHaveBeenCalledWith({ token })
+    expect(crypto.validateToken).toHaveBeenCalledTimes(1)
+  })
+})
