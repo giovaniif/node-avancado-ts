9207c804ef9bdfd679bdb6f9a6544978ba6f3f40
Giovani Farias <riccog.25@gmail.com>
2021-11-25 22:56:31 -0300

    test: ensure UserProfile creates with correct initials

--- tests/domain/entities/user-profile.spec.ts
+++ tests/domain/entities/user-profile.spec.ts
@@ -1,13 +1,67 @@
 import { UserProfile } from '@/domain/entities'
 
 describe('UserProfile', () => {
+  let sut: UserProfile
+
+  beforeEach(() => {
+    sut = new UserProfile('any_id')
+  })
   it('should create with empty initials when pictureUrl is provided', () => {
-    const sut = new UserProfile('any_id')
     sut.setPicture({ pictureUrl: 'any_url', name: 'any_name' })
 
     expect(sut).toEqual({
       id: 'any_id',
       pictureUrl: 'any_url',
+      initials: undefined
+    })
+  })
+
+  it('should create with empty initials when pictureUrl is provided', () => {
+    sut.setPicture({ pictureUrl: 'any_url' })
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: 'any_url',
+      initials: undefined
+    })
+  })
+
+  it('should create initials with firstLetter of first and last name', () => {
+    sut.setPicture({ name: 'Giovani Ricco Farias' })
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
+      initials: 'GF'
+    })
+  })
+
+  it('should create initials with first two letters of first name', () => {
+    sut.setPicture({ name: 'Giovani' })
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
+      initials: 'GI'
+    })
+  })
+
+  it('should create initials with first letter', () => {
+    sut.setPicture({ name: 'G' })
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
+      initials: 'G'
+    })
+  })
+
+  it('should create with empty initials when name and picture url are not provided', () => {
+    sut.setPicture({ })
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
       initials: undefined
     })
   })
