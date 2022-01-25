7356c77eab5a0f9c381cb80e019cbd58b9083e3e
Giovani Farias <riccog.25@gmail.com>
2022-01-24 22:47:17 -0300

    test: ensure DeletePictureController returns 204

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,12 +1,17 @@
 import { ChangeProfilePicture } from '@/domain/use-cases'
+import { HttpResponse, noContent } from '@/application/helpers'
+import { Controller } from '@/application/controllers'
 
 type HttpRequest = { userId: string }
 
-class DeleteProfilePictureController {
-  constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
+class DeleteProfilePictureController extends Controller {
+  constructor (private readonly changeProfilePicture: ChangeProfilePicture) {
+    super()
+  }
 
-  async handle ({ userId }: HttpRequest): Promise<void> {
+  async perform ({ userId }: HttpRequest): Promise<HttpResponse> {
     await this.changeProfilePicture({ id: userId })
+    return noContent()
   }
 }
 
@@ -24,10 +29,23 @@ describe('DeleteProfilePicture controller', () => {
     sut = new DeleteProfilePictureController(changeProfilePicture)
   })
 
+  it('should extend controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
+  })
+
   it('should call ChangeProfilePicture with correct input', async () => {
     await sut.handle({ userId })
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: userId })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 204', async () => {
+    const httpResponse = await sut.handle({ userId })
+
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: null
+    })
   })
 })
