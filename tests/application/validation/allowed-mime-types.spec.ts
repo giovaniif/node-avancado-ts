a161eb10886ea2c381cf2c007567edb4b3f58b11
Giovani Farias <gfarias@tractian.com>
2022-02-26 06:09:21 -0300

    refactor: move code to source

--- tests/application/validation/allowed-mime-types.spec.ts
+++ tests/application/validation/allowed-mime-types.spec.ts
@@ -1,26 +1,5 @@
 import { InvalidMimeTypeError } from '@/application/errors/validation'
-
-type Extension = 'png' | 'jpg'
-
-class AllowedMimeTypes {
-  constructor (private readonly allowed: Extension[], private readonly mimetype: string) {}
-
-  validate (): Error | undefined {
-    let isValid = false
-    if (this.isPng()) isValid = true
-    else if (this.isJpg()) isValid = true
-
-    if (!isValid) return new InvalidMimeTypeError(this.allowed)
-  }
-
-  private isPng (): boolean {
-    return this.allowed.includes('png') && this.mimetype === 'image/png'
-  }
-
-  private isJpg (): boolean {
-    return this.allowed.includes('jpg') && /image\/jpe?g/.test(this.mimetype)
-  }
-}
+import { AllowedMimeTypes } from '@/application/validation'
 
 describe('AllowedMimeTypes', () => {
   it('should return InvalidMimeTypeError if value is invalid', () => {
